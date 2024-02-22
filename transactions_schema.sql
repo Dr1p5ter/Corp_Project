14f7e85365bdf9c75b0c91e294e5fca0774ceba8
Matthew McLaren <Wildcatdud@gmail.com>
2024-02-22 14:47:33 -0500

    Update transactions_schema.sql
    modified the schema before further updates

--- transactions_schema.sql
+++ transactions_schema.sql
@@ -8,18 +8,13 @@ CREATE TABLE IF NOT EXISTS store_transactions (
     t_id INT NOT NULL AUTO_INCREMENT UNIQUE,
     user_id INT,
     game_id INT,
+    date_processed DATETIME NOT NULL,
+    refund_availability_before DATETIME NOT NULL,
     PRIMARY KEY (t_id),
     FOREIGN KEY (user_id)
         REFERENCES user_info.login_credentials (user_id),
-	FOREIGN KEY (game_id)
-		REFERENCES game_connections.game_info (game_id)
-);
-
-CREATE TABLE IF NOT EXISTS transaction_count_per_user (
-    user_id INT NOT NULL,
-    num_of_purchases INT,
-    FOREIGN KEY (user_id)
-        REFERENCES user_info.login_credentials (user_id)
+    FOREIGN KEY (game_id)
+        REFERENCES game_connections.game_info (game_id)
 );
 
 USE sys;
