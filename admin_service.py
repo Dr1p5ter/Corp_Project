e9022d88b3f418cff119f0866571d46cc75e306d
Matthew McLaren <Wildcatdud@gmail.com>
2024-02-27 09:41:18 -0500

    Update admin_service.py
    changed debugging section

--- admin_service.py
+++ admin_service.py
@@ -4,38 +4,39 @@ from sql_helpers import *
 from user_info_helpers import *
 
 if __name__ == "__main__" :
+    user_dict = {}
+    n_entries_to_gen = 25
+    start_at_one = True
+    user_id_begin_inc_at = 1
 
-    # TODO: fill in window here in future
+    for i in range(1 if start_at_one else 0, n_entries_to_gen + 1 if start_at_one else n_entries_to_gen) :
+        f, m, l = gen.gen_name()
+        username = gen.gen_username(f, l, minit = m, random_int_included = 1)
+        password = gen.gen_password()
+        key = "user_" + str(i)
 
-    ...
+        user_dict[key] = {
+            "user_id" : user_id_begin_inc_at,
+            "fname" : f,
+            "minit" : m,
+            "lname" : l,    
+            "login_username" : username,
+            "login_password" : password
+        }
+        user_id_begin_inc_at += 1
 
-    # user_dict = dict()
-    # n_entries_to_gen = 50
-    # start_at_one = True
-    # user_id_begin_inc_at = 1
+        try :
+            res = insert_user_credentials(username, password)
+            print(res) if res != None else ...
+        except Exception as err :
+            print(err)
+            user_dict.pop(key)
 
-    # for i in range(1 if start_at_one else 0, n_entries_to_gen + 1 if start_at_one else n_entries_to_gen) :
-    #     f, m, l = gen.gen_name()
-    #     username = gen.gen_username(f, l, minit = m, random_int_included = (int(random.choice(string.digits)) % 2))
-    #     password = gen.gen_password()
+    for key in user_dict.keys() :
+        print(f'{key} -> {user_dict[key]}')
 
-    #     user_dict[i] = {
-    #         "user_id" : user_id_begin_inc_at,
-    #         "fname" : f,
-    #         "minit" : m,
-    #         "lname" : l,
-    #         "login_username" : username,
-    #         "login_password" : password
-    #     }
-    #     user_id_begin_inc_at += 1
-
-    #     try :
-    #         print(insert_user_credentials(username, password))
-    #     except Exception :
-    #         user_dict.pop(i)
-
-    # print(update_user_credentials("new_me", new_username = "bad_user", new_password = "bad_pass"))
-    # print(update_user_credentials(user_dict[0]["login_username"], new_username = "new_user", new_password = "new_pass"))
-    # print(update_user_credentials(user_dict[1]["login_username"], new_password = "new_pass"))
-    # print(update_user_credentials(user_dict[2]["login_username"], new_username = "new_user"))
-    # print(update_user_credentials(user_dict[3]["login_username"]))
+    print(update_user_credentials("new_me", new_username = "bad_user", new_password = "bad_pass"))                              # warning no username found
+    print(update_user_credentials(user_dict["user_1"]["login_username"], new_username = "new_user", new_password = "new_pass")) # update user_1 with new pass and name
+    print(update_user_credentials(user_dict["user_2"]["login_username"], new_password = "new_pass"))                            # only change password
+    print(update_user_credentials(user_dict["user_3"]["login_username"], new_username = "new_user"))                            # only change username
+    print(update_user_credentials(user_dict["user_4"]["login_username"]))                                                       # warning no new cred given to function
