b55a9c98ff9a1b3abaf417ebd58f85c78a8b8b9a
Matthew McLaren <Wildcatdud@gmail.com>
2024-02-26 21:20:06 -0500

    Add descriptions of functions
    Also simplified function calls and variables.

--- generator.py
+++ generator.py
@@ -17,11 +17,31 @@ def gen_password(char_limit : int = 32) -> str :
     return "".join([random.choice(LET + DIG + SPE) for i in range(char_limit)])
 
 def gen_random_int(char_limit : int = 3) -> int :
-    return "".join([random.choice(string.digits) for i in range(char_limit)])
+    """ Generate random int suffix for username generation
+
+    Builds string of random digits within char limit
+
+    returns :
+        randomized integer string of digits
+    """
+    return "".join([random.choice(DIG) for i in range(char_limit)])
 
 def gen_username(firstname : str, lastname : str, char_limit : int = 32, minit : str = None, random_int_included : bool = 1) -> str :
+    """ Generate username of fixed length
+
+    Construct a username given a set name
+
+    returns :
+        uniformly generated username
+    """
     return str(firstname[0].lower() + (minit.lower() if minit != None else "") + lastname.lower() + (str(gen_random_int()) if random_int_included else ""))[0:char_limit]
 
 def gen_name() -> list[str, str, str] :
-    return (faker.Faker().unique.first_name(), random.choice(string.ascii_uppercase), faker.Faker().unique.last_name())
+    """ Generate fake name
 
+    Construct a fake name used for account generation for testing
+
+    returns :
+        tuple of first name, middle initial, and last name
+    """
+    return (faker.Faker().unique.first_name(), random.choice(LET), faker.Faker().unique.last_name())
