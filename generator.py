a872ad848003ff15d187cf005950a5f2eea43307
Matthew McLaren <Wildcatdud@gmail.com>
2024-02-26 20:35:00 -0500

    Bug fix
    Made functions more easy to use. Fixed error with key word in gen_password

--- generator.py
+++ generator.py
@@ -2,13 +2,13 @@ import faker
 import string
 import random
 
-def gen_password(char_limit : int = 16) -> str :
-    return "".join([random.choice(string.ascii_letters + string.digits + "!@#$%^&*_+-=~") for i in range(len)])
+def gen_password(char_limit : int = 32) -> str :
+    return "".join([random.choice(string.ascii_letters + string.digits + "!@#$%^&*_+-=~") for i in range(char_limit)])
 
-def gen_random_int(len : int = 3) -> int :
-    return "".join([random.choice(string.digits) for i in range(len)])
+def gen_random_int(char_limit : int = 3) -> int :
+    return "".join([random.choice(string.digits) for i in range(char_limit)])
 
-def gen_username(firstname : str, lastname : str, char_limit : int, minit : str = None, random_int_included : bool = 1) -> str :
+def gen_username(firstname : str, lastname : str, char_limit : int = 32, minit : str = None, random_int_included : bool = 1) -> str :
     return str(firstname[0].lower() + (minit.lower() if minit != None else "") + lastname.lower() + (str(gen_random_int()) if random_int_included else ""))[0:char_limit]
 
 def gen_name() -> list[str, str, str] :
