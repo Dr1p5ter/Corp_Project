ce5c3f308fbadbae22954bd622e14730081f9f36
Matthew McLaren <Wildcatdud@gmail.com>
2024-03-19 12:08:24 -0400

    Added datetime generator + bug fix

--- generator.py
+++ generator.py
@@ -2,11 +2,20 @@ import faker
 import string
 import random
 
+from datetime import datetime
+
 from user_info_helpers import UILC_LU_MAX_LEN, UILC_LP_MAX_LEN
 
 LET = string.ascii_letters # all ascii letters
 DIG = string.digits        # all ascii digits
 SPE = "!@#$%^&*_+-=~"      # all special ascii letters
+
+year_limit = (1945, 2024)  # range of datetime year
+month_limit = (1, 12)      # range of datetime month
+day_limit = (1, 31)        # range of datetime day
+hour_limit = (0, 23)       # range of datetime hour
+minute_limit = (0, 59)     # range of datetime minute
+second_limit = (0, 59)     # range of datetime second
 
 def gen_password(char_limit : int = UILC_LP_MAX_LEN) -> str :
     """ Generate random password of fixed length
@@ -46,4 +55,20 @@ def gen_name() -> list[str, str, str] :
     returns :
         tuple of first name, middle initial, and last name
     """
-    return (faker.Faker().unique.first_name(), random.choice(LET), faker.Faker().unique.last_name())
+    return (faker.Faker().unique.first_name(), random.choice(LET).upper(), faker.Faker().unique.last_name())
+
+def gen_datetime() -> datetime :
+    """ Generate a fake datetime
+
+    Construct a datetime that can be used for a fake bday date
+
+    returns :
+        datetime
+    """
+    yyyy = random.randrange(year_limit[0], year_limit[1] + 1)
+    mm = random.randrange(month_limit[0], month_limit[1] + 1)
+    dd = random.randrange(day_limit[0], day_limit[1] + 1)
+    HH = random.randrange(hour_limit[0], hour_limit[1] + 1)
+    MM = random.randrange(minute_limit[0], minute_limit[1] + 1)
+    SS = random.randrange(second_limit[0], second_limit[1] + 1)
+    return datetime(yyyy, mm, dd, HH, MM, SS)
