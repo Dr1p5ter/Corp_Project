72493ffa5680559d9e5fef9ad761124ae3b0c0de
Matthew McLaren <Wildcatdud@gmail.com>
2024-03-19 12:55:46 -0400

    Added Events for permission and profile updates
    More percentages and all of the percentages can be modified. Look at descriptions if curious

--- incrimentally_populate.py
+++ incrimentally_populate.py
@@ -11,8 +11,12 @@ known_records_path = "records.tsv"  # path to file holding all stored populated 
 known_records = {}                  # dictionary that holds in all of the records
 
 dpx = 5     # percentage of deletions
-upx = 15    # percentage of updates
-ipx = 80    # percentage of insertions
+upx = 35    # percentage of updates
+ipx = 60    # percentage of insertions
+
+uspx = 20   # percentage of updating strike count
+uppx = 60   # percentage of updating profile info
+uipx = 20   # percentage of updating login info
 
 tfx = 4    # coeficient that dictates rate of population time gaps differ from [0, tfx)
 
@@ -33,71 +37,105 @@ try :
                     "user_id" : vals[0],
                     "fname" : vals[1],
                     "minit" : vals[2],
-                    "lname" : vals[3],    
-                    "login_username" : vals[4],
-                    "login_password" : vals[5]
+                    "lname" : vals[3],
+                    "b_date" : vals[4],    
+                    "login_username" : vals[5],
+                    "login_password" : vals[6]
                 }
 except FileNotFoundError :
-    logger.info(colored("known_records_path doesn't exist within current directory, nothing added to known records\n", COL_CONTEXT))
+    logger.info(colored("known_records_path doesn't exist within current directory, nothing added to known records", COL_CONTEXT))
 
 # main loop for population
 while loop_end :
     # determine the behavior of randomized percentages
     x = floor(random.random() * 100)
     if (x >= 0) and (x < dpx) : # delete
-        print("delete")
-        if len(known_records) != 0 :
-            user_key = random.choice(list(known_records.keys()))
-            res = delete_user_credentials(known_records[user_key]["user_id"],
-                                          known_records[user_key]["login_username"],
-                                          known_records[user_key]["login_password"])
-            if res == None :
-                known_records.pop(user_key)
+        # nothing can be done if this is reached so skip
+        if len(known_records) == 0 :
+            continue
+
+        # grab a random key and delete the user
+        user_key = random.choice(list(known_records.keys()))
+        res = delete_user_credentials(known_records[user_key]["user_id"],
+                                        known_records[user_key]["login_username"],
+                                        known_records[user_key]["login_password"])
+        # if res is None and doesn't push an error then remove from records
+        if res == None :
+            known_records.pop(user_key)
     elif (x >= dpx) and (x < (dpx + upx)) : # update
-        print("update")
-        if len(known_records) != 0 :
-            user_key = random.choice(list(known_records.keys()))
+        # nothing can be done if this is reached so skip
+        if len(known_records) == 0 :
+            continue
+
+        # grab a random user key
+        user_key = random.choice(list(known_records.keys()))
+
+        # determine the behavior of the update
+        y = floor(random.random() * 100)
+        if (y >= 0) and (y < uspx) : # strike count update
+            # incriment the strike count and update
+            update_user_permissions(user_key, find_user_strike_count(user_key) + 1)
+        elif (y >= uspx) and (y < (uspx + uppx)) : # profile info update
+            # update profile with record provided credentials, not new
+            update_user_profile_info(user_key,
+                                     known_records[user_key]["fname"],
+                                     known_records[user_key]["minit"],
+                                     known_records[user_key]["lname"],
+                                     known_records[user_key]["b_date"])
+        elif (y >= (uspx + uppx)) and (y < (uspx + uppx + uipx)) : # credentials info update
+            # make a new username and password
             new_username = random.choice([None,
-                                          gen.gen_username(known_records[user_key]["fname"],
-                                                           known_records[user_key]["lname"],
-                                                           minit=known_records[user_key]["minit"],
-                                                           random_int_included=True)])
+                                        gen.gen_username(known_records[user_key]["fname"],
+                                                        known_records[user_key]["lname"],
+                                                        minit=known_records[user_key]["minit"],
+                                                        random_int_included=True)])
             new_password = random.choice([None, gen.gen_password()])
+
+            # update the credentials
             res = update_user_credentials(known_records[user_key]["login_username"],
-                                          new_username=new_username,
-                                          new_password=new_password)
+                                        new_username=new_username,
+                                        new_password=new_password)
+            
+            # if res is None then update the records
             if res == None :
                 if known_records[user_key]["login_username"] != new_username and new_username != None :
                     known_records[user_key]["login_username"] = new_username
                 if known_records[user_key]["login_password"] != new_password and new_password != None :
                     known_records[user_key]["login_password"] = new_password
     elif (x >= (dpx + upx)) and (x < (dpx + upx + ipx)) : # insert
-        print("insert")
+        # generate information for a known record
         fname, minit, lname = gen.gen_name()
         username = gen.gen_username(fname, lname, minit=minit, random_int_included=False)
         password = gen.gen_password()
+        b_date = gen.gen_datetime()
+
+        # insert the user's credentials
         res = insert_user_credentials(username, password)
+
+        # if successful then grab the user_id from the table and add to record
         if res == None :
             user_id = find_user_info(username)[0]
             known_records[str(user_id)] = {
                 "user_id" : user_id,
                 "fname" : fname,
                 "minit" : minit,
-                "lname" : lname,    
+                "lname" : lname,
+                "b_date" : b_date,
                 "login_username" : username,
                 "login_password" : password
             }
 
-    # Don't mess with this line
+    # nondetermanistic time incriments per record population
     time.sleep(random.random() * tfx)
 
 # record records in a tsv
 with open(known_records_path, "w", buffering = True) as file :
     for key in known_records.keys() :
-        file.write("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\n".format(known_records[key]["user_id"],
+        file.write("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\n".format(known_records[key]["user_id"],
                                                           known_records[key]["fname"],
                                                           known_records[key]["minit"],
                                                           known_records[key]["lname"],
+                                                          known_records[key]["b_date"],
                                                           known_records[key]["login_username"],
                                                           known_records[key]["login_password"]))
 file.close()
